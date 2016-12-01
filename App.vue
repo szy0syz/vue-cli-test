b08f7ec42b6bd070801ad8adca529cae42ce09bb
JerryShi <szy0syz@gmail.com>
2016-12-01 15:45:18 +0800

    init OK

--- App.vue
+++ App.vue
@@ -1,11 +1,28 @@
 <template>
-  <h1>welcome app.</h1>
+  <h1>welcome app</h1>
+  <hr>
+  <h2 @click="change">{{msg}}</hr>
 </template>
 
 <script>
 
+  export default {
+    data: function() {
+      return {
+        msg: 'webpack...'
+      }
+    },
+    methods: {
+    change: function() {
+        this.msg = 'changed!!!'  
+      }
+    }
+  }
+
 </script>
 
 <style>
-
+  body{
+		background: #fff
+  }
 </style>
