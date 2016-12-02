5bd6de65af0a91e33dd504e9e0e70b59ea508daf
JerryShi <szy0syz@gmail.com>
2016-12-02 16:42:46 +0800

    add extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports={
   entry: './main.js',
 
@@ -8,14 +10,18 @@ module.exports={
 
   module: {
     loaders:[
-      {test: /\.vue$/, loader: 'vue'},
-      {test: /\.js$/, loader: 'babel', exclude: /node_modules/}
+      { test: /\.vue$/, loader: 'vue' },
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader"), exclude: /node_modules/ }
     ]
   },
 
   babel: {
     presets: ['es2015'],
     plugins: ['transform-runtime']
-  }
+  },
 
+  plugins: [
+        new ExtractTextPlugin("styles.css")
+    ]
 };
